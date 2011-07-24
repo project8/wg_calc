67783213a4b3738d8f9f8409c76996b5fa6a019b
Jared Kofron <jared.kofron@gmail.com>
2011-07-24 15:00:57 -0500

    added a module for the parameters relevant to wr42 in general and some specific pieces as well

---
+++ WaveguideParameters.hs
@@ -0,0 +1,20 @@
+module WaveguideParameters where
+
+-- The Width, Length, and Depth of the KH3 NRAO amplifier
+kh3Width :: Integer
+kh3Width = 1800
+
+kh3Length :: Integer
+kh3Length = 1100
+
+kh3Depth :: Integer
+kh3Depth = 500
+
+-- The distance from flange face to centerline for Penn Engineering
+-- mitered waveguide bends
+castBendF2CL :: Integer
+castBendF2CL = 440
+
+-- The radius of WR42 flanges
+wr42FlangeHalfHeight :: Integer
+wr42FlangeHalfHeight = 438
